42ae664e7fa152428eb0a466ae46e8b1d3f9454d
Jamie Macey <jamie.git@tracefunc.com>
2014-04-29 16:51:45 -0700

    rails 3.2 encodes differently

--- spec/lib/quickbooks/service/base_service_spec.rb
+++ spec/lib/quickbooks/service/base_service_spec.rb
@@ -8,7 +8,7 @@ describe Quickbooks::Service::BaseService do
       subject.realm_id = 1234
       query = "SELECT * FROM Customer where Name = 'John'"
 
-      correct_url = "https://qb.sbfinance.intuit.com/v3/company/1234/query?query=SELECT+*+FROM+Customer+where+Name+%3D+%27John%27"
+      correct_url = "https://qb.sbfinance.intuit.com/v3/company/1234/query?query=SELECT%20*%20FROM%20Customer%20where%20Name%20=%20'John'"
       subject.url_for_query(query).should include(correct_url)
     end
 
